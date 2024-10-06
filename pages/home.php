c535606d1c23639dab713181cce68716282ad9a0
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-10-06 09:48:42 +0200

    Initial commit

---
+++ pages/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr-fr">
+
+    <?php require '../php/components/head.php' ?>
+
+    <body>
+        <?php require '../php/components/header.php' ?>
+
+        <main class="home_main">
+
+        </main>
+    </body>
+
+</html>
---
+++ php/components/head.php
@@ -0,0 +1,15 @@
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php if(isset($title)):?>
+            <?= $title; ?>
+        <?php else: ?>
+            Entreprise Fictive
+        <?php endif ?>
+    </title>
+    <link rel="icon" href="/src/img/logoCravoDavid.png">
+
+</head>
