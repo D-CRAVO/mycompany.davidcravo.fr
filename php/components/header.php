edfe850f648e79e12bf38ff4fb94a40d018373ba
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-10-08 08:10:05 +0200

    Changement du logo de l'entreprise

--- php/components/header.php
+++ php/components/header.php
@@ -2,11 +2,12 @@
 
 <header>
     <a href="../pages/home.php">
-        <img class="logo" src="/src/img/my_company.png" alt="Logo de l'entreprise">
+        <img class="logo" src="/src/img/my_company_logo.jpeg" alt="Logo de l'entreprise">
     </a>
     <nav>
         <ul>
             <?= nav_item('/pages/home.php', 'Acceuil') ?>
+            <?= nav_item('/pages/find_us.php', 'Nous trouver') ?>
         </ul>
     </nav>
 </header>
