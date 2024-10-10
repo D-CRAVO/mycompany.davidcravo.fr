2661ddf3efcaa31c703b3b419019abe2e2cf82a0
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-10-10 07:56:02 +0200

    Réorganisation des fichiers du dossier images

--- templates/find_us.php
+++ templates/find_us.php
@@ -1,51 +1,43 @@
 <?php 
-    include '../includes/init.php'; 
+    include __DIR__ . DIRECTORY_SEPARATOR . '../includes/init.php'; 
+    require __DIR__ . DIRECTORY_SEPARATOR . '../components/head.php';
+    require __DIR__ . DIRECTORY_SEPARATOR . '../components/header.php'; 
+    
     date_default_timezone_set('Europe/Paris');
     $time = (float)date('H:i');
     $time_slots = TIME_SLOTS[date('w')];
     $open = in_time_slots($time, $time_slots);
 ?>
 
-<!DOCTYPE html>
-<html lang="fr-fr">
+<main class="main_find_us">
 
-    <?php require '../components/head.php' ?>
-
-    <body>
-
-        <?php require '../components/header.php' ?>
-
-        <main class="main_find_us">
-
-        <div class="slots_addresses">
-            <img class="building" src="/assets/images/my_company_building.jpeg" alt="Photo du batiment">
-            <div class="right">
-                <div class="opening_hours">
-                    <h2>Horaires d'ouverture :</h2>
-                    <?php if($open): ?>
-                        <p class="open">L'entreprise est ouverte</p>
-                    <?php else: ?>
-                        <p class="close">L'entreprise est fermée</p>
-                    <?php endif ?>
-                    <ul>
-                        <?php foreach(DAYS as $k => $day): ?>
-                            <li class=<?= selected_date($k) ?>>
-                                <?= $day. ' : ' . time_slots_html(TIME_SLOTS[$k]) ?>
-                            </li>
-                        <?php endforeach ?>
-                    </ul>
-                </div>
-
-                <div class="address">
-                    <h2>Adresses :</h2>
-                    <?php foreach(ADDRESSES as $address): ?>
-                        <p> <?= addresses_html($address) ?> </p>
+    <div class="slots_addresses">
+        <img class="building" src="/assets/images/find_us/my_company_building.jpeg" alt="Photo du batiment">
+        <div class="right">
+            <div class="opening_hours">
+                <h2>Horaires d'ouverture :</h2>
+                <?php if($open): ?>
+                    <p class="open">L'entreprise est ouverte</p>
+                <?php else: ?>
+                    <p class="close">L'entreprise est fermée</p>
+                <?php endif ?>
+                <ul>
+                    <?php foreach(DAYS as $k => $day): ?>
+                        <li class=<?= selected_date($k) ?>>
+                            <?= $day. ' : ' . time_slots_html(TIME_SLOTS[$k]) ?>
+                        </li>
                     <?php endforeach ?>
-                </div>
-            </div>            
-             
-        </div>   
-        </main>
+                </ul>
+            </div>
+            <div class="address">
+                <h2>Adresses :</h2>
+                <?php foreach(ADDRESSES as $address): ?>
+                    <p> <?= addresses_html($address) ?> </p>
+                <?php endforeach ?>
+            </div>
+        </div>            
         
-    </body>
-</html>
+    </div>  
+
+</main>
+    
--- templates/home.php
+++ templates/home.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr-fr">
+<?php 
+    require __DIR__ . DIRECTORY_SEPARATOR . '../components/head.php';
+    require __DIR__ . DIRECTORY_SEPARATOR . '../components/header.php';
+?>
 
-    <?php require '../components/head.php' ?>
+<main class="home_main">
+    <img src="/assets/images/home/my_company_desk.jpeg" alt="photo d'un bureau équipé">
+</main>
 
-    <body>
-        <?php require '../components/header.php' ?>
-
-        <main class="home_main">
-            <img src="/assets/images/my_company_banner.jpeg" alt="photo d'un bureau équipé">
-        </main>
-    </body>
-
-</html>
